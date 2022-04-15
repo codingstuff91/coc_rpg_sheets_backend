06c4901453f4ea49cf821c097d33257093cbb336
Matthieu MARTIN <codingstuff91@gmail.com>
2022-04-15 21:27:10 +0200

    Uninstall sequelize and config

--- config/db.js
+++ config/db.js
@@ -1,16 +1,1 @@
-const { Sequelize } = require("sequelize");
 
-// Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("coc_character_sheet", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
-
-try {
-  sequelize.authenticate();
-  console.log("Connection has been established successfully.");
-} catch (error) {
-  console.error("Unable to connect to the database:", error);
-}
-
-module.export = sequelize;

edea1ebe8afc8f2ec6b30f30b8da18b36dd4ac43
Matthieu MARTIN <codingstuff91@gmail.com>
2022-04-15 22:02:41 +0200

    Delete index router

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+require("dotenv").config();
+
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var charactersRouter = require("./routes/characterRouter");
 
-const { sequelize } = require("./config/db.js");
-
+const PORT = process.env.PORT || 3000
 var app = express();
 
 app.use(logger("dev"));
@@ -16,7 +17,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/character", charactersRouter);
+
+app.listen(PORT, () => {
+    console.info(`Application fonctionne sur le port ${PORT}`);
+});
 
 module.exports = app;

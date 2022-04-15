27c281063cd5e7e445d225b007c31e9237788687
Matthieu MARTIN <codingstuff91@gmail.com>
2022-04-15 22:18:45 +0200

    Edit DB config file

--- config/database.js
+++ config/database.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 function connect(url) {
     return mongoose.connect(url, {
         useNewUrlParser: true,
-        useCreateIndex: true,
         useUnifiedTopology: true,
     });
 }
